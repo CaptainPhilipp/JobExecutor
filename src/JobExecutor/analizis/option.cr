3efa7a5c2b1d49ceb3090666149fd8c72fa75234
CaptainPhilipp <CaptainPhilipp@users.noreply.github.com>
2017-05-18 14:43:30 +0300

    style n comment

--- src/JobExecutor/analizis/option.cr
+++ src/JobExecutor/analizis/option.cr
@@ -1,12 +1,14 @@
 # Хранит данные опции для последующей проверки релевантности
 struct Option
-  getter full_name : String, requirement : Int32, name : String,
-         compare_trigger : String
-
+  getter full_name : String, requirement : Int32, name : String, compare_trigger : String
   getter? size_trigger : Bool, diff_trigger : Bool
 
-  def initialize(@full_name, @requirement, @name, @compare_trigger,
-                 @size_trigger, @diff_trigger)
+  def initialize(@full_name,        # max_childs_size
+                 @requirement,      # 10      ("max_childs_size" => 10)
+                 @name,             # childs  (_childs_)
+                 @compare_trigger,  # >       (max_)
+                 @size_trigger,     # true    (_size)
+                 @diff_trigger)     # false   (_diff)
   end
 
   alias key_name = full_name
